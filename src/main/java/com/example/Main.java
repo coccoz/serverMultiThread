91e6e25ac5620fafeda3af732b7452665c56a593
cosimo <ballerini.co@gmail.com>
2024-10-09 09:44:45 +0200

    testas

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,16 +1,34 @@
 package com.example;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        ServerSocket ss = new ServerSocket(5632);
-        Socket myS = ss.accept();
+        System.out.println("server in avvio");
+
+        ServerSocket server = new ServerSocket(3000);
+        Socket myS = server.accept();
 
         System.out.println("qualcuno si è collegato");
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(myS.getInputStream()));
+        DataOutputStream out = new DataOutputStream(myS.getOutputStream());
+
+        String recivedString = in.readLine();
+        System.out.println("stringa ricevuta: " + recivedString);
+
+        String upperRecivedString = recivedString.toUpperCase();
+        out.writeBytes(upperRecivedString + "\n");
+
+        myS.close();
+        server.close();
+
 
 
     }
