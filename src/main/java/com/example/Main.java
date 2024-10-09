a3a07ad9b762aa9da6fb75b2024091d57ed4e3e3
cosimo <ballerini.co@gmail.com>
2024-10-09 08:52:00 +0200

    test

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,15 @@
 package com.example;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+
+        ServerSocket ss = new ServerSocket(5637);
+        Socket myS = ss.accept();
+
+        System.out.println("qualcuno si è collegato");
     }
 }
